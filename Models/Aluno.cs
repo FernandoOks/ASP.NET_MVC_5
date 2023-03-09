353592370b3d8a0056a34fe962e342421774a958
Fernando <ferjah13@gmail.com>
2023-03-09 16:16:58 -0300

    DataAnnotations

---
+++ Models/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TesteMVC5.Models
+{
+    public class Aluno
+    {
+        public int id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        [EmailAddress(ErrorMessage ="Email em formato inválido")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string cpf { get; set; }
+
+        public DateTime dataMatricula { get; set; }
+        public bool ativo { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage ="As senha informadas não conferem")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string SenhaConfirmacao { get; set; }
+    }
+}
