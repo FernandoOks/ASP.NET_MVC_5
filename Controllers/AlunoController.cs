babd0958f6de41be6d61a3f3d40df2d7e70315bb
Fernando <ferjah13@gmail.com>
2023-03-09 20:13:54 -0300

    Validação de modelos

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteMVC5.Models;
+
+namespace TesteMVC5.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [Route("novo-aluno")]
+        public ActionResult Novo(Aluno aluno)
+        {
+            aluno = new Aluno
+            {
+                id = 1,
+                nome = "Eduardo",
+                cpf = "1234567895",
+                dataMatricula = DateTime.Now,
+                email = "teste@teste.com",
+                ativo = true
+            };
+
+            return RedirectToAction("Index", aluno);
+        }
+
+        public ActionResult Index(Aluno aluno)
+        {
+            if (!ModelState.IsValid) return View(aluno);
+
+            return View(aluno);
+        }
+    }
+}
