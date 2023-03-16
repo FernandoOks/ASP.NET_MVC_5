2310f2cf135512e682c129ad03118b3e7d0d7a94
Fernando <ferjah13@gmail.com>
2023-03-16 18:37:21 -0300

    View e Razor - Trabalhando com HtmlHelpers

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -10,22 +10,7 @@ namespace TesteMVC5.Controllers
     public class AlunoController : Controller
     {
         [Route("novo-aluno")]
-        public ActionResult Novo(Aluno aluno)
-        {
-            aluno = new Aluno
-            {
-                id = 1,
-                nome = "Eduardo",
-                cpf = "1234567895",
-                dataMatricula = DateTime.Now,
-                email = "teste@teste.com",
-                ativo = true
-            };
-
-            return RedirectToAction("Index", aluno);
-        }
-
-        public ActionResult Index(Aluno aluno)
+        public ActionResult NovoAluno(Aluno aluno)
         {
             if (!ModelState.IsValid) return View(aluno);
 
